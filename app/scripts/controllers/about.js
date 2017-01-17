d6ad1d728097ffd54da1c74d01564f8906893b9c
azalgorri <sepe.cursos2@gmail.com>
2017-01-17 18:30:41 +0100

    Inicio
    Inicio

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name msAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the msAppApp
+ */
+angular.module('msAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name msAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the msAppApp
+ */
+angular.module('msAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
